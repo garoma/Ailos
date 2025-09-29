b26be435db375b5100be119e25312e0c5cbab2ed
frank <franklin.garoma@gmail.com>
2025-09-29 13:53:20 -0300

    ...

--- Api/Domain/Entities/Tarifa.cs
+++ Api/Domain/Entities/Tarifa.cs
@@ -2,9 +2,9 @@
 {
     public class Tarifa
     {
-        public string IdTarifa { get; set; }
-        public string IdContaCorrente { get; set; }
-        public DateTime DataMovimento { get; set; }
-        public decimal Valor { get; set; }
+        public string IdTarifa { get; set; } //
+        public string IdContaCorrente { get; set; } //
+        public DateTime DataMovimento { get; set; } //
+        public decimal Valor { get; set; } //
     }
 }
